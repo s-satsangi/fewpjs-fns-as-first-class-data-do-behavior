8b0636944225b6dc08beb9f70a23828dd8617a53
s-satsangi <s.satsangi@gmail.com>
2020-11-17 20:47:14 -0500

    OMG this is starting to make a little sense!

--- index.js
+++ index.js
@@ -3,11 +3,31 @@
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById("time").value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  //take the string, remove the delimiter and turn it into an int
+  let message = "";
+  time = time.split(":");
+  time = time[0] + time[1];
+  time = parseInt(time);
+  //Great! Now set & return message based on time :)
+  time < 1200
+    ? (message = "Good Morning")
+    : time < 1700
+    ? (message = "Good Afternoon")
+    : time < 2400
+    ? (message = "Good Evening")
+    : (message = "Call Doc Brown, You're Outta Time!");
+  return message;
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message;
+}
